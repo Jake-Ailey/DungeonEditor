5fd39dc92040e9ad3c2465c9fce45333002b5ea3
Jake-Ailey <36465180+Jake-Ailey@users.noreply.github.com>
2018-08-10 11:46:23 +1000

    Added a live preview to the grid creation

--- CSTool/NewWindow.Designer.cs
+++ CSTool/NewWindow.Designer.cs
@@ -37,6 +37,8 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -65,7 +67,7 @@
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.SystemColors.MenuText;
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(11, 9);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
@@ -79,7 +81,7 @@
             this.textBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(12, 44);
+            this.textBox2.Location = new System.Drawing.Point(11, 50);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(115, 25);
@@ -90,27 +92,34 @@
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(132, 19);
+            this.textBox3.Location = new System.Drawing.Point(132, 13);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(42, 20);
             this.textBox3.TabIndex = 4;
             this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textBox3.TextChanged += new System.EventHandler(this.gridHeight_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
             // 
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(133, 49);
+            this.textBox4.Location = new System.Drawing.Point(132, 55);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(42, 20);
             this.textBox4.TabIndex = 5;
             this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textBox4.TextChanged += new System.EventHandler(this.gridWidth_TextChanged);
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.textBox6);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.textBox3);
             this.panel1.Location = new System.Drawing.Point(0, -2);
             this.panel1.Name = "panel1";
@@ -126,18 +135,37 @@
             this.panel2.TabIndex = 7;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(193, 9);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(91, 25);
+            this.textBox5.TabIndex = 6;
+            this.textBox5.Text = "Cell Size: ";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(290, 14);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(42, 20);
+            this.textBox6.TabIndex = 7;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            this.textBox6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox6_KeyPress);
+            // 
             // NewWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(879, 549);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
             this.Name = "NewWindow";
             this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.Text = "Project Creation";
@@ -145,7 +173,6 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -160,5 +187,7 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
--- CSTool/NewWindow.cs
+++ CSTool/NewWindow.cs
@@ -12,8 +12,9 @@ namespace CSTool
 {
     public partial class NewWindow : Form
     {
-        public int gridWidth;
         public int gridHeight;
+        public int gridWidth;
+        public int cellSize;
 
         public NewWindow()
         {
@@ -25,14 +26,87 @@ namespace CSTool
 
         }
 
+        //GRID HEIGHT textBox3
         private void gridHeight_TextChanged(object sender, EventArgs e)
         {
-            gridHeight = (int)sender;
+            //The textbox will only accept 2 digits, 
+            textBox3.MaxLength = 2;
+
+            if(textBox3.TextLength == 0)
+            {
+                gridHeight = 0;
+                return;
+            }
+
+            gridHeight = Int32.Parse(textBox3.Text);
+
+            if (gridWidth != 0 && cellSize != 0)
+                panel2.Refresh();
         }
 
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Only allows numeric input, and does not allow decimal places
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        //GRID WIDTH textBox4
         private void gridWidth_TextChanged(object sender, EventArgs e)
         {
-            gridWidth = (int)sender;
+            //The textbox will only accept 2 digits
+            textBox4.MaxLength = 2;
+
+            if (textBox4.TextLength == 0)
+            {
+                gridHeight = 0;
+                return;
+            }
+
+            gridWidth = Int32.Parse(textBox4.Text);
+
+            if(gridHeight != 0 && cellSize != 0)
+            panel2.Refresh();
+        }
+
+       
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Only allows numeric input, and does not allow decimal places
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        //CELL SIZE textbox6
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            //The textbox will only accept 2 digits
+            textBox6.MaxLength = 2;
+
+            if (textBox6.TextLength == 0)
+            {
+                cellSize = 0;
+                return;
+            }
+
+            cellSize = Int32.Parse(textBox6.Text);
+
+            if (gridHeight != 0 && gridWidth != 0)
+                panel2.Refresh();
+        }
+
+        //Function that only allows the user to enter in numerical keys
+        private void textBox6_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Only allows numeric input, and does not allow decimal places
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -42,21 +116,24 @@ namespace CSTool
 
         private void panel2_Paint(object sender, PaintEventArgs e)
         {
-            for (int i = 0; i < gridHeight; i++)
+            //Live preview of how big your grid will be
+
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.Black);
+
+            for (int y = 0; y < gridHeight; ++y)
             {
-                for (int j = 0; j < gridWidth; j++)
-                {
-                    Panel p = new Panel();
-                    p.Location = new Point(gridHeight * i + j, gridWidth * i + j);
-                    p.Size = new Size(16, 16);
-                    p.BorderStyle = BorderStyle.FixedSingle;
-                    p.Margin = new Padding(1, 1, 1, 1);
-                    p.Dock = DockStyle.None;
-                    p.BackColor = Color.White;
-
-
-                }
+                g.DrawLine(p, 0, y * cellSize, gridHeight * cellSize, y * cellSize);
             }
-        }
+            //Draws an extra line on the right to close off the grid
+            g.DrawLine(p, gridWidth * cellSize, 0, gridWidth * cellSize, gridHeight * cellSize);
+
+            for (int x = 0; x < gridWidth; ++x)
+            {
+                g.DrawLine(p, x * cellSize, 0, x * cellSize, gridWidth * cellSize);
+            }
+            //Draws an extra line on the bottom to close off the grid
+            g.DrawLine(p, 0, gridHeight * cellSize, gridWidth * cellSize, gridHeight * cellSize);
+        }       
     }
 }
